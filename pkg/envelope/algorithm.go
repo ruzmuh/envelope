dac87dc72447eb1e23c6b10a5a2d5d3fbd1c8af2
ruzmuh <ruzmuh@gmail.com>
2022-10-02 16:14:50 +0300

    change layout

---
+++ pkg/envelope/algorithm.go
@@ -0,0 +1,9 @@
+package envelope
+
+type Encrypter interface {
+	getID() string
+	encrypt(key, data []byte) (result []byte, err error)
+	decrypt(key, data []byte) (result []byte, err error)
+	MarshalCBOR() (data []byte, err error)
+	UnmarshalCBOR(data []byte) (err error)
+}
---
+++ pkg/envelope/envelope_test.go
@@ -0,0 +1,30 @@
+package envelope
+
+import (
+	"encoding/hex"
+	"fmt"
+	"testing"
+)
+
+func TestNewEnvelope(t *testing.T) {
+	key, err := hex.DecodeString("56e47a38c5598974bc46903dba290349")
+	if err != nil {
+		panic(err)
+	}
+	data, err := hex.DecodeString("a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf")
+	if err != nil {
+		panic(err)
+	}
+	envelope, _ := NewEnvelope("AES_128_CBC")
+	encrypted, _ := envelope.Encrypt(key, "AES_128_CBC", data)
+	fmt.Print(string(encrypted))
+
+	decrypted, e := Decrypt(key, encrypted)
+	if e != nil {
+		return
+	}
+	if hex.EncodeToString(data) != hex.EncodeToString(decrypted) {
+		t.Errorf("result must be = %s, not %s", data, encrypted)
+	}
+
+}
